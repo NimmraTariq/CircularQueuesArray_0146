e44bc8dcbe2fe03c116a58a5656cc7d2869211d6
NimmraTariq <nimra369tariq@gmail.com>
2025-06-10 11:50:31 +0700

    Displayed elements from FRONT to end

--- CircularQueues.cpp
+++ CircularQueues.cpp
@@ -65,3 +65,11 @@ class Queues {
         cout << "\nElements in the queue are... \n";
         cout << "\n Elements in the queue are: "; 
         if (FRONT_position <= REAR_position) {
+            while (FRONT_position <= REAR_position) {
+                cout << queue_array[FRONT_position] << " ";
+                FRONT_position++;
+            }
+            cout << endl;
+        } 
+        else {
+            while (FRONT_position <= max -1) { 
