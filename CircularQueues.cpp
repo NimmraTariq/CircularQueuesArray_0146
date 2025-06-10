36447e2e322ba1bee973a0acb57a71b887a93fc8
NimmraTariq <nimra369tariq@gmail.com>
2025-06-10 11:52:19 +0700

    Created object of Queues class

--- CircularQueues.cpp
+++ CircularQueues.cpp
@@ -79,3 +79,13 @@ class Queues {
 
             FRONT_position = 0; 
             while (FRONT_position <= REAR_position) {
+                cout << queue_array[FRONT_position] << " ";
+                FRONT_position++;
+            }
+            cout << endl; 
+        }
+    } 
+    }; 
+
+int main (){
+    Queues q;
