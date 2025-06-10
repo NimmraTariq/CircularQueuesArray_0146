0cc039a471d3e7dc5256b048814e74cff1c7ccd7
NimmraTariq <nimra369tariq@gmail.com>
2025-06-10 11:53:42 +0700

    Called insert function

--- CircularQueues.cpp
+++ CircularQueues.cpp
@@ -101,3 +101,8 @@ int main (){
             cout << "Enter your choice(1-4) : ";
 
             cin >> ch;
+            cout << endl;
+
+            switch (ch) {
+                case '1':
+                    q.insert();
