2d2da18de6b466fb133334563007f2e1ebf3e0e7
NimmraTariq <nimra369tariq@gmail.com>
2025-06-10 11:51:03 +0700

    Reset position for wrap-around display

--- CircularQueues.cpp
+++ CircularQueues.cpp
@@ -73,3 +73,8 @@ class Queues {
         } 
         else {
             while (FRONT_position <= max -1) { 
+                cout << queue_array[FRONT_position] << " ";
+                FRONT_position++;
+            }
+
+            FRONT_position = 0; 
